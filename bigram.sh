18818a2632e82281c49d9df06f734a202470b374
Hanhong Xue <macroxue1@gmail.com>
2018-03-27 18:25:53 -0400

    Add bidirectional bigram.

--- bigram.sh
+++ bigram.sh
@@ -1,3 +1,7 @@
-for key in ';' a b c d e f g h i j k l m n o p q r s t u v w x y z; do
-  echo $key $(grep -o "${key}[a-z;]" code.txt | cut -c2 | sort | uniq -c | sort -nr)
+keys="; a b c d e f g h i j k l m n o p q r s t u v w x y z"
+for key in $keys; do
+  echo $key "->" $(grep -o "${key}[a-z;]" code.txt | cut -c2 | sort | uniq -c | sort -nr)
+done
+for key in $keys; do
+  echo $key "<-" $(grep -o "[a-z;]${key}" code.txt | cut -c1 | sort | uniq -c | sort -nr)
 done
